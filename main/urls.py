6ae3de4a7e5af85215f464a0f82a59172a7480b8
me1ky <ivkoshkin17@gmail.com>
2026-05-03 02:27:25 +0300

    Добавлены views и urls для приложения main

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('prices/', views.prices, name='prices'),
+    path('contacts/', views.contacts, name='contacts'),
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def services(request):
+    return render(request, 'main/services.html')
+
+def prices(request):
+    return render(request, 'main/prices.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
